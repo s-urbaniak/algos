e029cb74d0fcc3891ab282f35c66f02c86aa9447
Sergiusz Urbaniak <sergiusz.urbaniak@ebay.com>
2014-06-07 19:37:40 +0200

    refactorings

--- dijkstra/dijkstra.go
+++ dijkstra/dijkstra.go
@@ -5,7 +5,7 @@ import (
 	"errors"
 )
 
-const maxInt = ^uint(0)
+const inf = ^uint(0)
 
 type Edge struct {
 	u, v int
@@ -13,10 +13,13 @@ type Edge struct {
 }
 
 type Vertex struct {
+	i      int
 	adj    []int
 	label  string
+	dist   uint
+
+	// heap invariants
 	key    uint
-	inHeap bool
 	pos    int
 }
 
@@ -26,52 +29,16 @@ type Graph struct {
 	heap   VertexHeap
 }
 
-type HeapEntry int
-
 type VertexHeap struct {
-	heap   []HeapEntry
-	vertex []Vertex
-	dist   []uint
+	heap   []*Vertex
 }
 
-func NewVertex(label string, edges ...int) Vertex {
-	return Vertex{edges, label, 0, false, -1}
+func NewVertex(i int, label string, edges ...int) Vertex {
+	return Vertex{i, edges, label, inf, 0, -1}
 }
 
 func NewEdge(u, v int, len uint) Edge {
 	return Edge{u, v, len}
-}
-
-func NewGraph(V []Vertex, s int, E []Edge) (*Graph, error) {
-	if len(V) <= s {
-		return nil, errors.New("unknown source vertex")
-	}
-
-	// create heap, add all vertices
-	H := make([]HeapEntry, len(V))
-	for i, _ := range H {
-		V[i].key = maxInt
-		V[i].inHeap = true
-		H[i] = HeapEntry(i)
-	}
-
-	// calculate keys for adjacent vertices of source vertex s
-	for _, i := range V[s].adj {
-		if E[i].u == s {
-			v := E[i].v
-			if E[i].len < V[v].key {
-				V[v].key = E[i].len
-			}
-		}
-	}
-
-	// remove source vertex s
-	H = append(H[:s], H[s+1:]...)
-	V[s].inHeap = false
-
-	A := make([]uint, len(V))
-	h := VertexHeap{H, V, A}
-	return &Graph{V, E, h}, nil
 }
 
 func min(x, y uint) uint {
@@ -81,28 +48,64 @@ func min(x, y uint) uint {
 	return y
 }
 
+func NewGraph(V []Vertex, s int, E []Edge) (*Graph, error) {
+	if len(V) <= s {
+		return nil, errors.New("unknown source vertex")
+	}
+
+	// create heap, add all vertices
+	H := make([]*Vertex, len(V))
+	for i, _ := range H {
+		// initialize heap invariants
+		V[i].pos = i
+		V[i].key = inf
+		V[i].dist = inf
+		H[i] = &V[i]
+	}
+
+	// calculate keys for adjacent vertices of source vertex s
+	for _, i := range V[s].adj {
+		if E[i].u == s {
+			v := E[i].v
+			V[v].key = min(V[v].key, E[i].len)
+		}
+	}
+
+	// remove source vertex s
+	V[s].pos = -1
+	V[s].dist = 0
+	H = append(H[:s], H[s+1:]...)
+
+	return &Graph{V, E, VertexHeap{H}}, nil
+}
+
 func (g Graph) DoShortestPath() {
 	heap.Init(&g.heap)
 	for g.heap.Len() > 0 {
-		wStar := heap.Pop(&g.heap).(HeapEntry)
-		g.heap.dist[wStar] = g.vertex[wStar].key
+		wStar := heap.Pop(&g.heap).(*Vertex)
+		wStar.dist = wStar.key
 
-		for _, i := range g.vertex[wStar].adj {
-			u := g.edge[i].u
-			v := g.edge[i].v
+		for _, i := range wStar.adj {
+			edge := g.edge[i]
+			u := edge.u
+			v := &g.vertex[edge.v]
 
-			if (u == int(wStar)) && (g.vertex[v].inHeap) {
-				g.vertex[v].key = min(
-					g.vertex[v].key,
-					g.vertex[wStar].key+g.edge[i].len)
-				heap.Fix(&g.heap, g.vertex[v].pos)
+			if (u == wStar.i) && (v.pos >= 0) {
+				v.key = min(
+					v.key,
+					wStar.key+edge.len)
+				heap.Fix(&g.heap, v.pos)
 			}
 		}
 	}
 }
 
 func (g Graph) GetDist() []uint {
-	return g.heap.dist
+	dist := make([]uint, len(g.vertex))
+	for i, v := range g.vertex {
+		dist[i] = v.dist
+	}
+	return dist
 }
 
 func (h VertexHeap) Len() int {
@@ -110,14 +113,12 @@ func (h VertexHeap) Len() int {
 }
 
 func (h VertexHeap) Less(i, j int) bool {
-	return h.vertex[h.heap[i]].key < h.vertex[h.heap[j]].key
+	return h.heap[i].key < h.heap[j].key
 }
 
 func (h VertexHeap) Swap(i, j int) {
-	vi := &h.vertex[h.heap[i]]
-	vj := &h.vertex[h.heap[j]]
-	vi.pos = j
-	vj.pos = i
+	h.heap[i].pos = j
+	h.heap[j].pos = i
 	h.heap[i], h.heap[j] = h.heap[j], h.heap[i]
 }
 
@@ -126,18 +127,18 @@ func (h *VertexHeap) Pop() interface{} {
 	n := len(old)
 	x := old[n-1]
 
-	h.vertex[x].inHeap = false
+	x.pos = -1
 	h.heap = old[0 : n-1]
 
 	for i, entry := range h.heap {
-		h.vertex[entry].pos = i
+		entry.pos = i
 	}
 
 	return x
 }
 
 func (h *VertexHeap) Push(x interface{}) {
-	he := x.(HeapEntry)
-	h.vertex[he].pos = len(h.heap)
+	he := x.(*Vertex)
+	he.pos = len(h.heap)
 	h.heap = append(h.heap, he)
 }
